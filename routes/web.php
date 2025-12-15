e665b05722209441d13f6132f71215e4304494bc
muhamadgumilang <muhamadgumilang68@gmail.com>
2025-12-15 11:51:05 +0700

    tugas

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//contoh
+Route::get('/tentang', function () {
+    return view('tentang');
+});
+
+
+//latihan 1
+Route::get('/sapa/{nama}', function ($nama) {
+   return "Halo, $nama! Selamat datang di Toko Online.";
+});
+
+
+//latihan 2
+Route::get('/kategori/{nama?}', function ($nama = 'Semua') {
+    return "Kategori komik: $nama";
+});
